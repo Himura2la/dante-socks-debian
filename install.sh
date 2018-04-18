18df2f3274ccb11ceb20e4610d5810f1c09b411e
Himura Kazuto <glagol15@gmail.com>
2018-04-18 15:19:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
+echo This file should be used only for reference, do not run it
+return
+
 dpkg -i dante_1.4.2-1_amd64.deb
 
 useradd -s /usr/sbin/nologin -M socksuser
-echo Enter a password used when connecting to the proxy
+echo Enter a password used to connect to the proxy
 passwd socksuser
 
-touch /var/run/sockd.pid
+> /var/run/sockd.pid
 chown socksuser:socksuser /var/run/sockd.pid
 
-cp sockd.conf /etc/sockd.conf
+mv sockd.conf /etc/sockd.conf
 
-cp sockd.service /etc/systemd/system/sockd.service
+mv sockd.service /etc/systemd/system/sockd.service
 systemctl daemon-reload
+
+service sockd status
 
